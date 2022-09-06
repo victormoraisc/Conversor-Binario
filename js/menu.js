8e05bca41e799b6689889f84abe2126585939ba2
victormoraisc <93659822+victormoraisc@users.noreply.github.com>
2022-09-05 21:17:51 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
+//Class just for organization of the code
+
 class Menu {
     constructor(menu, menuList, menuLinks){
         this.menu = document.querySelector(menu);
@@ -7,6 +9,7 @@ class Menu {
         this.handleClick = this.handleClick.bind(this);
     }
 
+    //Function that animate the links when it apear on page
     animateLinks()
     {
         this.menuLinks.forEach((link, index) =>
@@ -16,17 +19,19 @@ class Menu {
             : (link.style.animation = `itemListFade 0.5s ease forwards ${index/7 + 0.3}s`);
         })
     }
+    //Function that toggle the menu
     handleClick()
     {
         this.menuList.classList.toggle(this.activeClass);
         this.menu.classList.toggle(this.activeClass);
         this.animateLinks();
     }
+    //Event listener that trigger menu
     addClickEvent()
     {
         this.menu.addEventListener("click", this.handleClick);
     }
-
+    //Function that start the code
     init()
     {
         if (this.menu)
@@ -36,6 +41,7 @@ class Menu {
         return this;
     }
 }
+//Build the class in a variable
 const menu = new Menu
 (
     ".sandwitch_menu",
@@ -43,5 +49,5 @@ const menu = new Menu
     ".item_list li"
 
 )
-
+//Start the code by the variable
 menu.init();
